050030e25c7a9ca3f5a4a7c1f79ff4918f975ff1
jeongukim <sum9310@gmail.com>
2022-05-16 12:46:40 +0900

    monster3 logic complete

--- Speedrun/Assets/Player/PlayerMove.cs
+++ Speedrun/Assets/Player/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float runSpeed;
