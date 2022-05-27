06b2bcf3aaa4b37a155a4c427d499e18650257a8
jeongukim <sum9310@gmail.com>
2022-05-27 16:06:45 +0900

    맵 다 그림

---
+++ Speedrun/Assets/Monster/Scripts/monster2behave.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster2behave : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
