ee7d5f948ce2c228bc05fcea200ebbdde7cb7a2e
JEBIN <ghkdwpqls@naver.com>
2022-06-06 16:57:00 +0900

    4차수정
    배경 화면 변경
    게임 끝나고 랭킹 화면 올라오게 수정
    아직 못한 것
    - 게임 시작 전 추락하는 현상
    ingame(true)상태인데 뭐가 문제인지 모르겠음. 게임 시작하자마자 player가 안움직이도록 해야하나 싶음
    게임 한판하고 메인화면으로 돌아온 뒤 게임을 다시 시작하면 초기화가 안됨

--- Speedrun/Assets/UI/Code/GameController.cs
+++ Speedrun/Assets/UI/Code/GameController.cs
@@ -7,13 +7,13 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public static bool GameIsPaused = false;
+    public GameObject pauseMenuCanvas;
 
     public int hitPoints;
-    public GameObject hudContainer, gameOverPanel;
-    public GameObject OptionUI;
+    public GameObject hudContainer, gameOverPanel, OptionUI;
     public Text timeCounter, countdownText;
     public bool gamePlaying { get; private set;}
-    static int countdownTime = 3;
+    public int countdownTime;
     public GameObject finishObj;
 
     private float startTime, elapsedTime;
@@ -26,7 +26,6 @@ public class GameController : MonoBehaviour
     public GameObject RankUI;
     public GameObject RankLayoutUI;
     public GameObject RankPrefab;
-    public GameObject FinishUI;
     ///////////////
     public Text FinishTimeTxt;
 
@@ -52,7 +51,6 @@ public class GameController : MonoBehaviour
         MainMenuUI.SetActive(true);
         OptionUI.SetActive(false);
         RankUI.SetActive(false);
-        FinishUI.SetActive(false);
     }
     public void ActiveRankUI()
     {
@@ -62,54 +60,34 @@ public class GameController : MonoBehaviour
 
         rankSetting();
     }
-    List<GameObject> RankList = new List<GameObject>();
     private void rankSetting()
     {
         RankingSystem rank = new RankingSystem();
         float[] timeArr = rank.GetAllRankingTime();
         string[] dateArr = rank.GetAllRankingDate();
-        if (RankList.Count == 0)
+        for (int i =0; i < 10; i++)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                GameObject obj = Instantiate(RankPrefab);
-                obj.transform.SetParent(RankLayoutUI.transform);
-                Text[] textArr = obj.GetComponentsInChildren<Text>();
-                textArr[0].text = (i + 1).ToString();
-                textArr[1].text = dateArr[i];
-           
-
-                TimeSpan timespan = TimeSpan.FromSeconds(timeArr[i]);
-                textArr[2].text = timeArr[i] > 99999 ? "" : timespan.ToString("mm':'ss'.'ff"); ;
-                RankList.Add(obj);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Text[] textArr = RankList[i].GetComponentsInChildren<Text>();
-                textArr[0].text = (i + 1).ToString();
-                textArr[1].text = dateArr[i];
-                textArr[2].text = timeArr[i] > 99999 ? "" : timeArr[i].ToString();
-            }
+            GameObject obj = Instantiate(RankPrefab);
+            obj.transform.SetParent(RankLayoutUI.transform);
+            Text[] textArr = obj.GetComponentsInChildren<Text>();
+            textArr[0].text = (i+1).ToString();
+            textArr[1].text = dateArr[i];
+            textArr[2].text = timeArr[i] > 99999 ? "" : timeArr[i].ToString();
         }
     }
     private void BeginGame()
     {
         gamePlaying = true;
         startTime = Time.time;
-        //게임 시작하고 카운트다운끝났을때
     }
     Coroutine countCor;
     public void StartGame()
     {
-        //스타트는 게임화면 넘어오자마자
-        gamePlaying = false;
-        Time.timeScale = 1f;
-        countdownText.gameObject.SetActive(true);
         MainMenuUI.SetActive(false);
         InGameUI.SetActive(true);
+
+        StartCoroutine(CountdownToStart());
+
         timeCounter.text = "00:00.00";
         OptionUI.SetActive(false);
        
@@ -128,7 +106,22 @@ public class GameController : MonoBehaviour
     RankingSystem s;
     private void Update()
     {
-     
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            s.SaveRanking(testtime);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log(s.GetRankingIndex(testtime));
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            s.ResetRanking();
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //s.DebugRanking();
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
            Time.timeScale = 0;
@@ -152,25 +145,17 @@ public class GameController : MonoBehaviour
             timeCounter.text = timePlayingStr; //r게임시작하면 0초에서 시작
         }
     }
-    public void ActiveOption()
-    {
-        Debug.Log(OptionUI.transform.Find("option").gameObject);
-        OptionUI.SetActive(!OptionUI.activeSelf);
-        OptionUI.transform.Find("option").gameObject.SetActive(OptionUI.activeSelf);
-        OptionUI.transform.Find("ingame").gameObject.SetActive(false);
-    }
+
     public void Resume()
     {
-        OptionUI.SetActive(false);
-        OptionUI.transform.Find("ingame").gameObject.SetActive(false);
+        pauseMenuCanvas.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     public void Pause(){
-
-        OptionUI.SetActive(true);
-        OptionUI.transform.Find("ingame").gameObject.SetActive(true);
+        
+            pauseMenuCanvas.SetActive(true);
             Time.timeScale = 0f;
             GameIsPaused = true;
         
@@ -185,14 +170,13 @@ public class GameController : MonoBehaviour
     {
         gamePlaying = false;
         FinishTimeTxt.text = timePlaying.ToString("mm':'ss'.'ff");
-        Invoke("ShowFinishScreen", 1.25f);
         Invoke("ShowFinishRanking", 3.5f);
       
         s.SaveRanking((float)timePlaying.TotalSeconds);
         
 
     }
-    private void ShowFinishScreen()
+        private void ShowFinishScreen()
         {
             gameOverPanel.SetActive(true);
             hudContainer.SetActive(false);
@@ -208,12 +192,11 @@ public class GameController : MonoBehaviour
     }
     IEnumerator CountdownToStart()
     {
-        int tmptime = countdownTime;
-        while (tmptime > 0)
+        while (countdownTime > 0)
         {
-            countdownText.text = tmptime.ToString();
+            countdownText.text = countdownTime.ToString();
             yield return new WaitForSeconds(1f);
-            tmptime--;
+            countdownTime--;
         }
 
         BeginGame();
