030f5f0d66956baa9000504bc204b22459ed1a0b
JEBIN <ghkdwpqls@naver.com>
2022-06-07 11:26:15 +0900

    Revert "4차수정"
    This reverts commit ee7d5f948ce2c228bc05fcea200ebbdde7cb7a2e.

--- Speedrun/Assets/UI/Code/GameController.cs
+++ Speedrun/Assets/UI/Code/GameController.cs
@@ -7,13 +7,13 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public static bool GameIsPaused = false;
-    public GameObject pauseMenuCanvas;
 
     public int hitPoints;
-    public GameObject hudContainer, gameOverPanel, OptionUI;
+    public GameObject hudContainer, gameOverPanel;
+    public GameObject OptionUI;
     public Text timeCounter, countdownText;
     public bool gamePlaying { get; private set;}
-    public int countdownTime;
+    static int countdownTime = 3;
     public GameObject finishObj;
 
     private float startTime, elapsedTime;
@@ -26,6 +26,7 @@ public class GameController : MonoBehaviour
     public GameObject RankUI;
     public GameObject RankLayoutUI;
     public GameObject RankPrefab;
+    public GameObject FinishUI;
     ///////////////
     public Text FinishTimeTxt;
 
@@ -51,6 +52,7 @@ public class GameController : MonoBehaviour
         MainMenuUI.SetActive(true);
         OptionUI.SetActive(false);
         RankUI.SetActive(false);
+        FinishUI.SetActive(false);
     }
     public void ActiveRankUI()
     {
@@ -60,34 +62,54 @@ public class GameController : MonoBehaviour
 
         rankSetting();
     }
+    List<GameObject> RankList = new List<GameObject>();
     private void rankSetting()
     {
         RankingSystem rank = new RankingSystem();
         float[] timeArr = rank.GetAllRankingTime();
         string[] dateArr = rank.GetAllRankingDate();
-        for (int i =0; i < 10; i++)
+        if (RankList.Count == 0)
         {
-            GameObject obj = Instantiate(RankPrefab);
-            obj.transform.SetParent(RankLayoutUI.transform);
-            Text[] textArr = obj.GetComponentsInChildren<Text>();
-            textArr[0].text = (i+1).ToString();
-            textArr[1].text = dateArr[i];
-            textArr[2].text = timeArr[i] > 99999 ? "" : timeArr[i].ToString();
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject obj = Instantiate(RankPrefab);
+                obj.transform.SetParent(RankLayoutUI.transform);
+                Text[] textArr = obj.GetComponentsInChildren<Text>();
+                textArr[0].text = (i + 1).ToString();
+                textArr[1].text = dateArr[i];
+           
+
+                TimeSpan timespan = TimeSpan.FromSeconds(timeArr[i]);
+                textArr[2].text = timeArr[i] > 99999 ? "" : timespan.ToString("mm':'ss'.'ff"); ;
+                RankList.Add(obj);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Text[] textArr = RankList[i].GetComponentsInChildren<Text>();
+                textArr[0].text = (i + 1).ToString();
+                textArr[1].text = dateArr[i];
+                textArr[2].text = timeArr[i] > 99999 ? "" : timeArr[i].ToString();
+            }
         }
     }
     private void BeginGame()
     {
         gamePlaying = true;
         startTime = Time.time;
+        //게임 시작하고 카운트다운끝났을때
     }
     Coroutine countCor;
     public void StartGame()
     {
+        //스타트는 게임화면 넘어오자마자
+        gamePlaying = false;
+        Time.timeScale = 1f;
+        countdownText.gameObject.SetActive(true);
         MainMenuUI.SetActive(false);
         InGameUI.SetActive(true);
-
-        StartCoroutine(CountdownToStart());
-
         timeCounter.text = "00:00.00";
         OptionUI.SetActive(false);
        
@@ -106,22 +128,7 @@ public class GameController : MonoBehaviour
     RankingSystem s;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            s.SaveRanking(testtime);
-        }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            Debug.Log(s.GetRankingIndex(testtime));
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            s.ResetRanking();
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            //s.DebugRanking();
-        }
+     
         if (Input.GetKeyDown(KeyCode.Escape))
         {
            Time.timeScale = 0;
@@ -145,17 +152,25 @@ public class GameController : MonoBehaviour
             timeCounter.text = timePlayingStr; //r게임시작하면 0초에서 시작
         }
     }
-
+    public void ActiveOption()
+    {
+        Debug.Log(OptionUI.transform.Find("option").gameObject);
+        OptionUI.SetActive(!OptionUI.activeSelf);
+        OptionUI.transform.Find("option").gameObject.SetActive(OptionUI.activeSelf);
+        OptionUI.transform.Find("ingame").gameObject.SetActive(false);
+    }
     public void Resume()
     {
-        pauseMenuCanvas.SetActive(false);
+        OptionUI.SetActive(false);
+        OptionUI.transform.Find("ingame").gameObject.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     public void Pause(){
-        
-            pauseMenuCanvas.SetActive(true);
+
+        OptionUI.SetActive(true);
+        OptionUI.transform.Find("ingame").gameObject.SetActive(true);
             Time.timeScale = 0f;
             GameIsPaused = true;
         
@@ -170,13 +185,14 @@ public class GameController : MonoBehaviour
     {
         gamePlaying = false;
         FinishTimeTxt.text = timePlaying.ToString("mm':'ss'.'ff");
+        Invoke("ShowFinishScreen", 1.25f);
         Invoke("ShowFinishRanking", 3.5f);
       
         s.SaveRanking((float)timePlaying.TotalSeconds);
         
 
     }
-        private void ShowFinishScreen()
+    private void ShowFinishScreen()
         {
             gameOverPanel.SetActive(true);
             hudContainer.SetActive(false);
@@ -192,11 +208,12 @@ public class GameController : MonoBehaviour
     }
     IEnumerator CountdownToStart()
     {
-        while (countdownTime > 0)
+        int tmptime = countdownTime;
+        while (tmptime > 0)
         {
-            countdownText.text = countdownTime.ToString();
+            countdownText.text = tmptime.ToString();
             yield return new WaitForSeconds(1f);
-            countdownTime--;
+            tmptime--;
         }
 
         BeginGame();
